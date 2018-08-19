7d9cbccc3e980b415be61e5a1beceb217e4e3b96
Abhinav Mishra <abhinav9032@gmail.com>
2018-08-19 23:03:44 +0530

    Skeleton Insertion without complex JS
    Skeleton Insertion without complex JS..Before data insertion or && population checking

--- src/App.js
+++ src/App.js
@@ -2,34 +2,67 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
+let defaultTextColor="#fff";
+let defaulTextStyle = {
+    color:defaultTextColor
+};
 
 class Aggregrate extends Component {
-    render(){       
+    render(){           
         return(  
-            
-        <h1>Developed and maintained by Abhinav Mishra</h1>
-        
+            <div style={{...defaulTextStyle,width:'40%',display:'inline-block'}}>
+                <h2 style={{display:'inline-block'}}>Number Text</h2>
+            </div>
         );       
     } 
 }
 
+class Filter extends Component {
+    render(){
+        return(
+            <div style={{...defaulTextStyle}}>
+                <img/>
+                <input type="search"/>  &nbsp;Filter
+            </div>        
+        );       
+    }    
+}
+
+
+class PlayList extends Component{
+    render(){
+        return(
+            <div style={{...defaulTextStyle,width:"25%",display:'inline-block'}}>
+                <img/>
+                <h3>Play List</h3>
+                <ul>
+                    <li>Song 1</li>
+                    <li>Song 2</li>
+                    <li>Song 3</li>
+                </ul>
+            </div>     
+        );    
+    }
+}
 class App extends Component {  
 render() {
-//let name='Abhinav' 
-//let MyHeader ={color:'red', 'font-size':'50px'}
-var title='Advanced Playlist'
-return (
-    <div className="App">
-    
-        <div className="TitleBox">
-            <h1>{title}</h1>
+        var title='Advanced Playlist'
+        return (
+            <div className="App">
+
+                <div className="TitleBox">
+                    <h1>{title}</h1>
+                </div>
+
+            <Aggregrate/>
+            <Aggregrate/>
+            <Filter/>
+            <PlayList/>
+            <PlayList/>
+            <PlayList/>
+            <PlayList/>
+
         </div>
-    
-    <Aggregrate/>
-    
-</div>
 );
 }
 }
