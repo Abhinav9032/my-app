7c48fc702f0ee0068575869bf69c508213c0b74e
Abhinav Mishra <abhinav9032@gmail.com>
2018-08-19 15:47:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+class Aggregrate extends Component {
+    render(){       
+        return(  
+            
+        <h1>Number Text 1</h1>
+        
+        );       
+    } 
+}
+
+class App extends Component {  
+render() {
+//let name='Abhinav' 
+//let MyHeader ={color:'red', 'font-size':'50px'}
+var title='Advanced Playlist'
+return (
+    <div className="App">
+    
+        <div className="TitleBox">
+            <h1>{title}</h1>
+        </div>
+    
+    <Aggregrate/>
+    
+</div>
+);
+}
+}
+
+export default App;
